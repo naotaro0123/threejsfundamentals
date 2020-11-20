cf33d9d63c1d68268e16ffeb18fd485f537f5cfa
naotaro0123 <n.ikewada@gmail.com>
2020-11-20 00:28:44 +0000

    Deploying to gh-pages from  @ c9ab7a32256c8f85f7137084aa467941b0b301af 🚀

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,10 @@
 
 'use strict';
 
+process.on('unhandledRejection', up => {
+  throw up;
+});
+
 const fs = require('fs');
 const path = require('path');
 const semver = require('semver');
@@ -130,6 +134,8 @@ module.exports = function(grunt) {
     siteName: 'ThreeJSFundamentals',
     siteThumbnail: 'threejsfundamentals.jpg',  // in rootFolder/lessons/resources
     templatePath: 'build/templates',
+    owner: 'gfxfundamentals',
+    repo: 'threejsfundamentals',
     thumbnailOptions: {
       thumbnailBackground: 'threejsfundamentals-background.jpg',
       text: [
